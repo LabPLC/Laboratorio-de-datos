227a4390372d419a30be3ada7309101dfbeb6829
poguez <zeugop@gmail.com>
2015-03-03 01:54:13 -0600

    Added plugin class

---
+++ ckanext/labplc/plugin.py
@@ -0,0 +1,23 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as tk
+
+class labplcPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config, 'templates')
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_public_directory(config, 'public')
+
+        # Add this plugin's fanstatic dir.
+        tk.add_resource('fanstatic', 'ckanext-labplc')
+
+    def before_map(self, m):
+        m.redirect('/', '/dataset')
+        return m
